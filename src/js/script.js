14c1da980f1ae4655e229a762047ea4ec8c86dbc
oluuwkn <145891312+oluuwkn@users.noreply.github.com>
2025-09-03 17:46:36 +0500

    2 версия

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,33 @@
-
-
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('.button');
 
 let expression = '';
+let isResultShown = false; 
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const value = button.textContent;
-   
-    if (value === '=') {
+
+    if (value === 'Del') {
+      expression = '';
+      display.value = '';
+      isResultShown = false; 
+    } 
+    else if (value === '=') {
+      try {
         expression = eval(expression).toString();
         display.value = expression;
-    } else {
-      expression += value;
-      display.value = expression;
+        isResultShown = true; 
+      } catch {
+        display.value = "Ошибка";
+        expression = '';
+      }
+    } 
+    else {
+      if (!isResultShown) { 
+        expression += value;
+        display.value = expression;
+      }
     }
   });
 });
-
